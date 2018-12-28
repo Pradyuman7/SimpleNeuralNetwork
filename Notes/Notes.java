5521860765768fd3f996ce2739cc812f8d4a226c
Pradyuman Dixit <crvasu779@gmail.com>
2018-12-28 08:45:12 +0100

    Create Notes.java

---
+++ Notes/Notes.java
@@ -0,0 +1,30 @@
+
+public class Notes {
+	
+	/*
+	 * 
+	 * Pattern for neural network to recognize is the XOR operation. 
+	 * The XOR operator truth table is shown below for the operation y= x1 XOR x2
+	 * 
+	 * 1. Increasing the number of nodes will increase the number of rows of our weight matrix.
+	 * 2. Increasing the number of features will increase the number of columns of matrices.
+	 * 
+	 * Activation function 
+	 * 	
+	 * 	sig(x) = 1/(1+e^(-x))
+	 * 
+	 * 	Z = wx + b
+	 * 
+	 * 	J(a,y) = -/sum^{m}_{i=1} (y^(i)log(a^(i)) + (1-y^(i))log(1-a^(i)))
+	 * 
+	 * 	dZ/dw = X
+	 * 
+	 * 	da/dx = (1-a)a
+	 * 
+	 * 	dJ/da = -y/a + (1-y)/(1-a)
+	 * 
+	 * 	db = (A - Y)
+	 * 
+	 */
+
+}
